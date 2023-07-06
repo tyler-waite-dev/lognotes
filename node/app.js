651149dc2985bb1ea0199548e5e05e9a18b93d27
twaite2 <74149348+twaite2@users.noreply.github.com>
2023-07-06 07:56:44 -0700

    Testing PG connection

--- node/app.js
+++ node/app.js
@@ -2,10 +2,43 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+const { Client } = require("pg")
+
+const dotenv = require("dotenv")
+dotenv.config()
+
+const pgConnected = false;
+
+
+const client = new Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT
 })
 
-app.listen(port, () => {
-  console.log(`Lognotes: listening on port ${port}`)
+client.connect((err) => {
+    if (err) {
+        console.log("pg connection error", err.stack)
+    } else {
+        console.log("pg connected")
+        pgConnected = true;
+    }
 })
+    const response = await client.query('SELECT * FROM some_table')
+    
+    app.get('/', (req, res) => {
+        client.query('SELECT * FROM some_table', (err, result) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(result.rows)
+                res.send(result.rows)
+            }
+        });
+    })
+    
+    app.listen(port, () => {
+        console.log(`Lognotes: listening on port ${port}`)
+    })
